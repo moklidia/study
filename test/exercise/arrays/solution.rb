d634702d7a8de3f6d7952b4efb6ce0f2ab3c757c
moklidia <kitsunegakkou@gmail.com>
2019-08-04 20:31:54 +0400

    complete array functions

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |el| el > 0 ? 100 : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        first = 0
+        last = array.length - 1
+
+        while first <= last
+          mid = (first + last) / 2
+          guess = array[mid]
+
+          if guess == query
+            return mid
+          elsif guess > query
+            last = mid - 1
+          else
+            first = mid + 1
+          end
+        end
+        -1
       end
     end
   end
