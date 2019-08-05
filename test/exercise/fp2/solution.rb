6bf8b097fa6f69e71dd88a25e5fe19f18d89fe15
moklidia <kitsunegakkou@gmail.com>
2019-08-05 18:13:11 +0400

    add my_reduce and fix lint

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,7 +6,7 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
-        i= 0
+        i = 0
         while i < count
           yield(self[i])
           i += 1
@@ -17,19 +17,22 @@ module Exercise
       # Написать свою функцию my_map
       def my_map
         result = MyArray.new([])
-        my_each{ |el| result << yield(el) }
-        return result
+        my_each { |el| result << yield(el) }
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
         result = MyArray.new([])
-        my_each { |el| result << el if !el.nil? }
-        return result
+        my_each { |el| result << el unless el.nil? }
+        result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = 0)
+        acc += 1 if yield(3, 2) == 3 * 2 && acc == 0
+        my_each { |el| acc = yield(acc, el) }
+        acc
       end
     end
   end
