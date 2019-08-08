bd1eff79e6a100bdc0a259880e8ff7c38ecef42c
moklidia <kitsunegakkou@gmail.com>
2019-08-08 10:28:05 +0400

    add rack routing

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,25 @@
 module Inatra
   class << self
-    def routes
+
+    @@routes = {}
+    def routes(&block)
+      instance_eval(&block)
+    end
+
+    def method_missing(method, args, &block)
+      append(method, args, &block)
+    end
+
+    def append(verb, path, &handler)
+      @@routes[verb] = {}
+      @@routes[verb][path] = handler
     end
 
     def call(env)
+      verb = env['REQUEST_METHOD'].downcase.to_sym
+      path = env['PATH_INFO']
+      handler = @@routes[verb][path]
+      handler.call
     end
   end
 end
--- test/exercise/rackup/my_app.rb
+++ test/exercise/rackup/my_app.rb
@@ -1,5 +1,6 @@
 Inatra.routes do
   get '/hello' do
+    # puts env
     [200, {}, ['Hello World']]
   end
 end
